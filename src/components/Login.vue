46f2f29186e68072295f524e00f9ed54612d34b9
Thomas <mosset.thomas@gmail.com>
2023-02-23 19:18:09 +0100

    feat: login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,104 @@
 <template>
-  <div class="Login">
-    <p>
-    Login
-    </p>
-  </div>
+  <v-form 
+    v-model="valid"
+    @submit.prevent="login(user)"
+    ref="form"
+    class="my-14"
+  >
+      <v-row class="text-h4 ma-10 d-flex justify-center">
+        Login
+      </v-row>
+
+      <v-row class="ma-8 d-flex justify-center">
+        <v-col
+          cols="8"
+        >
+          <v-text-field
+            v-model="user.email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+
+         <v-col
+          cols="8"
+        >
+          <v-text-field
+            v-model="user.password"
+            :rules="passwordRules"
+            label="Password"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-btn 
+        type="submit" 
+        color="button" 
+        class="mt-2"
+        @click="validate"
+        :disabled="!valid"
+      >Submit</v-btn>
+  </v-form>
+
+<!-- error message if loggin is incorrect -->
+  <v-row 
+    v-if="loginMessageError !==''" 
+    class="d-flex justify-center"
+  >
+    <v-col cols="6">
+      <v-alert
+        density="compact"
+        type="warning"
+        :text="loginMessageError"
+      ></v-alert>
+    </v-col>
+  </v-row>
+
+   
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'TheLogin',
+  data: () => ({
+    valid: false,
+    user: {
+      email: '',
+      password: '',
+    },
+    emailRules: [
+      value => {
+        if (value) return true
+
+        return 'E-mail is required.'
+      },
+      value => {
+        if (/.+@.+\..+/.test(value)) return true
+
+        return 'E-mail must be valid.'
+      },
+    ],
+    passwordRules: [
+      value => {
+        if (value) return true
+
+        return 'Password is required.'
+      },
+    ],
+  }),
+
+  methods: {
+    ...mapActions(['login']),
+  },
+
+  computed: {
+    ...mapGetters(['loginMessageError']),
+  },
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,59 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    users: [
+      {
+        id: 0,
+        pseudo: "userTest",
+        email: "user-0@gmail.com",
+        password: "pipou42",
+        notes: [],
+      },
+      {
+        id: 1,
+        pseudo: "superuser",
+        email: "superuser@gmail.com",
+        password: "bebou97",
+        notes: [],
+      },
+    ],
+    islogged : false,
+    loggedUser : [],
+    loginMessageError : "",
   },
   getters: {
+    loginMessageError (state) {
+      return state.loginMessageError;
+    },
   },
   mutations: {
+    LOGIN (state, user) {
+      // empty error message
+      state.loginMessageError = "";
+
+      for (let index = 0; index < state.users.length; index++) {
+        const userInSate = state.users[index];
+
+        if (userInSate.email === user.email) {
+
+          if (userInSate.password === user.password) {
+            state.islogged = true;
+            state.loggedUser.push(user);
+  
+            // empty error message
+            state.loginMessageError = "";
+          } else {
+            state.loginMessageError = "Email and/or password incorrect !";
+          }
+        } else {
+          state.loginMessageError = "Email and/or password incorrect !";
+        }
+      }
+    }
   },
   actions: {
+    login (context, user) {
+      context.commit('LOGIN', user);
+    }
   },
-  modules: {
-  }
 })
